a8ccfbe8a0aa5f7481e3d03490984bac0d2c0037
meri <mariana_asavoaiei@outlook.com>
2023-01-31 21:46:53 +0000

    created the long and lat variables and put together the queryURL.

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// creating a city variable that would reflect the chosen city
+let userCity;
+// the Api key from Open Weather
+let APIKey = "3592b3a1aeb58bf421b617073b74810f";
+//created the latitude and longitude variables that are going to take an give us the city.  
+let lat;
+let long;
+// creating the query url
+queryURL = "https://openweathermap.org/forecast5" + userCity + APIKey;
+
